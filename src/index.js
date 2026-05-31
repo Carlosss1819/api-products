3f6af22b2a73c39b00da8d8be94abc690f92f8ca
Carloss1819 <kastrocarloss@gmail.com>
2026-05-31 15:11:45 -0600

    fix: agregar prefijo /products a las rutas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
+
 import app from "./app.js";
 import "./database.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
